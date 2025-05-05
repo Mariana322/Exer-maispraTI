00696948fd5fb8fc139beb4961e2cd182609b148
marigimenez@hotmail.com <marigimenez@hotmail.com>
2025-05-05 11:29:01 -0300

    Adiciona comentários explicativos ao exercício 1

--- Exercicio1.js
+++ Exercicio1.js
@@ -1,9 +1,15 @@
-prompt = require('prompt-sync')();
+// Importa a biblioteca 'prompt-sync' para permitir entrada de dados pelo console
+const prompt = require('prompt-sync')(); 
 
+// Solicita ao usuário que digite um número e converte a entrada para o tipo Number
 let numero = Number(prompt("Digite um número: "));
 
+// Verifica se o número é divisível por 2 (ou seja, se é par)
 if (numero % 2 === 0) {
+    // Se o número for par, exibe a mensagem correspondente
     console.log("O número é par.");
 } else {
+    // Caso contrário (ímpar), exibe outra mensagem
     console.log("O número é ímpar.");
 }
+
